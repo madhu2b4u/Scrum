1b501de4a0c78fee298d78d4bb9f26c45a358764
Madhu <raydriiger@gmail.com>
2021-03-11 12:03:43 +1300

    Initial Commit

---
+++ Scrum/ScrumApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumApp.swift
+//  Scrum
+//
+//  Created by Madhu Kalyan Mattaparthi on 11/03/21.
+//
+
+import SwiftUI
+
+@main
+struct ScrumApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
